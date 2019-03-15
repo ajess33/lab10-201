0bc574f107c1ab187e3be4e33f2581b975b510b8
Austin Jess <ajess33@gmail.com>
2019-03-15 13:32:23 -0500

    first functions passes

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
+  // return array where first element is the sum of the numbers and the second is a concatened string "The sum of 4 and 7 is 11."
+  var total = a + b;
+  console.log([total, `The sum of ${a} and ${b} is ${total}.`]);
+  return [total, `The sum of ${a} and ${b} is ${total}.`];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
